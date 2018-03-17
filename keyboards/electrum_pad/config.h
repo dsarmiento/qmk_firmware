33f6792962adbde69d320f958a5603a0976b7fe9
Daniel Sarmiento <dsarmiento@users.noreply.github.com>
2018-03-16 23:14:10 -0500

    Add layers 1 & 2

--- keyboards/electrum_pad/config.h
+++ keyboards/electrum_pad/config.h
@@ -10,7 +10,7 @@
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Electrum
-#define PRODUCT         Pad
+#define PRODUCT         GamePad
 #define DESCRIPTION     Keyboard
 
 /* key matrix size */
