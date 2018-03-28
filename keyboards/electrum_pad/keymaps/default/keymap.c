6824d12b01e099bd2941b281d5998f43db26ca56
Daniel Sarmiento <dsarmiento@users.noreply.github.com>
2018-03-27 21:04:06 -0500

    Move F14

--- keyboards/electrum_pad/keymaps/default/keymap.c
+++ keyboards/electrum_pad/keymaps/default/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     { KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   },
     { KC_F15 , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   },
     { KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   },
-    { KC_LCTL, MO(1)  , KC_LALT, KC_F14 , XXXXXXX, KC_SPC }
+    { KC_LCTL, MO(1)  , KC_LALT, KC_F16 , XXXXXXX, KC_SPC }
 },
 
 /*
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     { _______, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   },
     { _______, _______, _______, _______, KC_VOLU, KC_I   },
     { _______, _______, _______, _______, KC_VOLD, _______},
-    { _______, _______, _______, _______, KC_MUTE, KC_M   },
+    { _______, _______, _______, KC_F14 , KC_MUTE, KC_M   },
     { _______, _______, _______, MO(2)  , XXXXXXX, _______} 
 },
 
